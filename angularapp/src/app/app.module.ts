e3167d37b0536c3485f75081745802da3f9e9642
Meenaloshini-002 <meenaloshini.e002@gmail.com>
2025-04-08 08:43:32 +0000

    first commit

---
+++ angularapp/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { UseraddcartComponent } from './components/useraddcart/useraddcart.component';
+import { UseraddfeedbackComponent } from './components/useraddfeedback/useraddfeedback.component';
+import { UserviewordersComponent } from './components/uservieworders/uservieworders.component';
+import { UserviewproductComponent } from './components/userviewproduct/userviewproduct.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RegistrationComponent,
+    UseraddcartComponent,
+    UseraddfeedbackComponent,
+    UserviewordersComponent,
+    UserviewproductComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ angularapp/src/app/components/useraddcart/useraddcart.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-useraddcart',
+  templateUrl: './useraddcart.component.html',
+  styleUrls: ['./useraddcart.component.css']
+})
+export class UseraddcartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
