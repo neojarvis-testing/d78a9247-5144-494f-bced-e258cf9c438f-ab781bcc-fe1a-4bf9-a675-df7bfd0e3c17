db4515b4497ad784dd3d92f6087ebb877999c5d0
Vishal-Saravanan <vishal.programming@gmail.com>
2025-04-08 08:56:53 +0000

    8/4/25 2:25 added few components

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminaddproductComponent } from './components/adminaddproduct/adminaddproduct.component';
+import { AdminnavbarComponent } from './components/adminnavbar/adminnavbar.component';
+import { AdminviewfeedbackComponent } from './components/adminviewfeedback/adminviewfeedback.component';
+import { AdminviewordersComponent } from './components/adminvieworders/adminvieworders.component';
+import { AdminviewproductComponent } from './components/adminviewproduct/adminviewproduct.component';
+import { AdminviewuserdetailsComponent } from './components/adminviewuserdetails/adminviewuserdetails.component';
+import { ErrorComponent } from './components/error/error.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminaddproductComponent,
+    AdminnavbarComponent,
+    AdminviewfeedbackComponent,
+    AdminviewordersComponent,
+    AdminviewproductComponent,
+    AdminviewuserdetailsComponent,
+    ErrorComponent,
+    HomeComponent,
+    LoginComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
---
+++ angularapp/src/app/components/adminaddproduct/adminaddproduct.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-adminaddproduct',
+  templateUrl: './adminaddproduct.component.html',
+  styleUrls: ['./adminaddproduct.component.css']
+})
+export class AdminaddproductComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
