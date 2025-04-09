a1e7d3f1ac5fa249525f5f418644a356883f950d
shagunrajput02 <shagunchauhan622@gmail.com>
2025-04-09 06:11:49 +0000

    09-04 11:40pm Created Feedback model, repository, service, service Impl and controller

--- springapp/src/main/java/com/examly/springapp/model/Feedback.java
+++ springapp/src/main/java/com/examly/springapp/model/Feedback.java
@@ -1,5 +1,31 @@
 package com.examly.springapp.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
 public class Feedback {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long feedbackld;
+    private String message;
+    private int rating;
+    
+    @ManyToOne
+    @JoinColumn(name = "userId")
+    private User user; 
 
 }
