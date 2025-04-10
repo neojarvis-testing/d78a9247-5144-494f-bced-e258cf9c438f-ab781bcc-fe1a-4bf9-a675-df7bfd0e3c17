92c27e4ca8eb7fb161f17728582e58bb4b73b22a
Meenaloshini-002 <meenaloshini.e002@gmail.com>
2025-04-10 05:25:37 +0000

    10/4/2025 - Added Product admin, User view product and admin view Product and added CrossOrigin

--- angularapp/src/app/components/adminviewproduct/adminviewproduct.component.ts
+++ angularapp/src/app/components/adminviewproduct/adminviewproduct.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-adminviewproduct',
@@ -7,9 +9,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminviewproductComponent implements OnInit {
 
-  constructor() { }
+  products: any[] = [];
+  filteredProducts: any[] = [];
+  categories: string[] = [];
+  selectedCategory: string = 'All';
+
+  constructor(private productService: ProductService, private router: Router) {}
 
   ngOnInit(): void {
+    this.getProducts();
   }
+
+  getProducts(): void {
+    this.productService.getProducts().subscribe(data => {
+      this.products = data;
+      this.filteredProducts = data;
+      this.categories = [...new Set(this.products.map(product => product.category))];
+    });
+  }
+
+  filterByCategory(): void {
+    if (this.selectedCategory === 'All') {
+      this.filteredProducts = this.products;
+    } else {
+      this.filteredProducts = this.products.filter(product => product.category === this.selectedCategory);
+    }
+  }
+
+  navigateToEdit(productId: number): void {
+    this.router.navigate(['/edit-product', productId]);
+  }
+
+  updateProduct(productId: number, product: any): void {
+    this.productService.updateProduct(productId,product).subscribe(() => {
+      alert('Data updated successfully!');
+      this.getProducts();
+    });
+  }
+
+  deleteProduct(productId: number): void {
+    if (confirm('Are you sure you want to delete this product?')) {
+      this.productService.deleteProduct(productId).subscribe(() => {
+        alert('Product deleted successfully!');
+        this.getProducts();
+      });
+    }
+  }
+
+  toggleAvailability(productId: number, product: any): void {
+    product.isAvailable = !product.isAvailable;
+    this.updateProduct(productId, product);
+  }
+
 
 }
